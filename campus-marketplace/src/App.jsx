344dc6bc238e3902d3c096090fb786f32781862d
Azmat Ali <azmat082003@gmail.com>
2025-10-11 20:58:53 +0500

    just for safty

--- campus-marketplace/src/App.jsx
+++ campus-marketplace/src/App.jsx
@@ -3,20 +3,18 @@ import LandingPage from "./LandingPage";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Class from "./Class";
+ 
 
 function App() {
   return (
-    <>
-    <Class />
-    </>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<LandingPage />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/signup" element={<Signup />} />
-    //   </Routes>
-    // </Router>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
